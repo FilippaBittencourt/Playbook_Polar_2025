792e0d029c9b42ac37c497b791b9640598da16f7
jorgeprj <jlmp2002@gmail.com>
2025-08-08 15:37:44 -0300

    fix: update logout handling to clear cookies and redirect to login

--- src/components/AdminHeader.tsx
+++ src/components/AdminHeader.tsx
@@ -7,7 +7,8 @@ export function AdminHeader() {
   const navigate = useNavigate();
 
   const handleLogout = () => {
-    navigate("/logout", { replace: true });
+        document.cookie = 'chaveSecreta=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT';
+        window.location.href = '/login';
   };
 
   return (
